820e5ba6c6fadc63127e6a8a4bec9d11c3b824d8
Tony24986 <115228257+Tony-24986@users.noreply.github.com>
2025-11-01 15:15:18 +0700

    bluetooth command added
    something something something

--- 4wd_pain.ino
+++ 4wd_pain.ino
@@ -1,17 +1,26 @@
+
 #include <SoftwareSerial.h>
 #define pA 5
 #define pB 6
 #define pC 9
 #define pD 10
+#define softRX 12
+#define softTX 13
 
-void setup(){
-	Serial.begin(9600);
-	pinMode(pA, OUTPUT);
-	pinMode(pB, OUTPUT);
-	pinMode(pC, OUTPUT);
-	pinMode(pD, OUTPUT);
+SoftwareSerial bt(softRX, softTX);
+bool arr[4+3] = 0;
+
+void setup() {
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  Serial.begin(9600);
+  bt.begin(19200);
 }
 
-void loop(){
-
+void loop() {
+  if(bt.availible()>0){
+    String st = bt.read();
+  }
 }
